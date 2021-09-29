842b07c2c11aade437f0aa12292f942faadc1495
Justyna Bodurka <bodurkajustyna@gmail.com>
2021-09-29 14:05:00 +0200

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,25 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+const entry=localStorage.getItem('entry');
+let result ='';
+if(entry){
+    result=entry;
+}
+
+const textArea = document.querySelector(".textarea--js");
+const save = document.querySelector(".button-right--js");
+save.addEventListener('click',() => {
+    localStorage.setItem('entry', textarea.value);
+})
+const load = document.querySelector(".button-left--js");
+load.addEventListener('click', ()=> {
+    localStorage.getItem('entry');
+})
+
+    
+
+     
+
+
+
